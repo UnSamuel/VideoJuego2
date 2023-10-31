e49507247fac09c8b2b459a2cb9a4cb693539399
UnSamuel <60000867swim@gmail.com>
2023-10-31 17:55:05 -0400

    subiendo proyecto

---
+++ The Last Dance First Try/The Last Dance First Try/Models/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoJuegos.Models
+{
+    public class Email
+    {
+        [Key]
+        public int Idemail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string DireccionEmail { get; set; }
+
+        public int Idusuario { get; set; }
+    }
+}
